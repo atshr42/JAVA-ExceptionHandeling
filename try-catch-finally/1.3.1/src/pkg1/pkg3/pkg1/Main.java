ca5fb4c4fd8470e2d269730f66db9c1ad3320bb9
atshr42 <aashish.stha42@gmail.com>
2021-01-08 15:46:52 +0545

    final

---
+++ try-catch-finally/1.3.1/src/pkg1/pkg3/pkg1/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1.pkg3.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aashish
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int  a,b;
+               double c;
+       Scanner input = new Scanner(System.in);      
+       System.out.println("Enter the value of two numbers to perform Division");
+       a = input.nextInt();
+       b = input.nextInt();
+       try{
+           c = (a/b);
+           System.out.println(c + " is the Result");
+       }
+       Catch(exception e){
+           System.out.println("Dividing operation has  been performed");
+       }
+        
+    }
+    
+}
